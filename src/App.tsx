60df7332faa10a99fb3e19f06fa867195756ec77
Arghya Santra <arghyasantra103@gmail.com>
2025-07-03 17:59:53 +0530

    some environment variables changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/tasks";
+const API_URL = import.meta.env.VITE_API_URL;
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
